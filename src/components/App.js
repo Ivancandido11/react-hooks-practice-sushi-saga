4067095c54b7dd18215e33fd59dd4240928b04fd
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-14 18:55:51 -0400

    completed Assignement

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [allSushi, setAllSushi] = useState([])
+  const [plates, setPlates] = useState([])
+  const [start, setStart] = useState(0)
+  const [end, setEnd] = useState(4)
+  const [balance, setBalance] = useState(100)
+  const displaySushi = allSushi.slice(start, end)
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(data => setAllSushi(data))
+  }, [])
+
+  const handleMoreClick = () => {
+    setStart(start => start + 4)
+    setEnd(end => end + 4)
+  }
+  const handleEatSushi = (piece) => {
+    setPlates([...plates, piece])
+  }
+  const handlePrice = (price) => {
+    if (price <= balance) {
+      setBalance(balance => balance - price)
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer
+        balance={balance}
+        sushi={displaySushi}
+        onPlateClick={handleEatSushi}
+        onMoreClick={handleMoreClick}
+        onHandlePrice={handlePrice}
+      />
+      <Table 
+        plates={plates}
+        balance={balance} 
+      />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,21 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushi, onPlateClick, onMoreClick, onHandlePrice, balance }) {
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushi.map(piece => <Sushi 
+        balance={balance}
+        key={piece.id}
+        onHandlePrice={onHandlePrice}
+        onPlateClick={onPlateClick} 
+        piece={piece} 
+        />)
+      }
+      <MoreButton 
+        onMoreClick={onMoreClick}
+      />
     </div>
   );
 }
